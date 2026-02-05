fd774ca30ac1e9ae5e92bb8cbf802f0538fd1cea
Sanika25 <sanikazapke@gmail.com>
2026-02-05 23:32:08 +0530

    Create Dialect class with validation methods

---
+++ dialect/base.py
@@ -0,0 +1,18 @@
+lass Dialect:
+    def allowed_statements(self):
+        return []
+
+    def max_subquery_depth(self):
+        return 1
+
+    def forbidden_keywords(self):
+        return []
+
+    def validate_statement(self, stmt, tokens):
+        return []
+
+    def validate_clauses(self, stmt, tokens):
+        return []
+
+    def validate_ddl(self, stmt, tokens):
+        return []
