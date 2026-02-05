29fb06f4632448fd614e09a22c990bcba5095ef4
Sanika25 <sanikazapke@gmail.com>
2026-02-05 19:00:59 +0530

    Change import from write_json_report to write_jsonf

---
+++ io_layer/__init__.py
@@ -0,0 +1,10 @@
+"""
+IO Layer package.
+
+Handles:
+- Reading SQL input from files or folders
+- Writing validation results to output files
+"""
+
+from .reader import read_input
+from .writer import write_jsonf
